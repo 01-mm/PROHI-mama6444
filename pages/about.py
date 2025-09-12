0a6c4451e0f143756719b2e4c1eddd9446a2731a
01-mm <matic.matija.mm@gmail.com>
2025-09-12 14:41:46 +0200

    Update About.py
    Changed the way the text is formatted. Only heading is in markdown. Left incomplete, need to verify information from last semester's Jupyter notebook.

--- pages/about.py
+++ pages/about.py
@@ -1,7 +1,8 @@
 import streamlit as st 
 st.title("Matija Matic")
 st.markdown("""
-    ## DSHI Project Information ##      
-    In the data science course taken during the spring semester of 2025, I built prediction models to predict whether a wholesale customer was from the HORECA or Retail channel.        
-            
+    ## DSHI Project Information ##             
             """)
+"""In the data science course taken during the spring semester of 2025, I built prediction models
+to predict whether a Portuguese retailer's wholesale customers came from the HORECA or Retail 
+channel based on their spending on [Add specific food category - look at Jupyter notebook!!!]."""
