64b9730209d27f4a287740b19f83a30ba3ce69cb
01-mm <matic.matija.mm@gmail.com>
2025-09-12 15:31:00 +0200

    Updated About page
    Added information about the DSHI project and summarized what I did in less than 150 words

--- pages/about.py
+++ pages/about.py
@@ -2,7 +2,12 @@ import streamlit as st
 st.title("Matija Matic")
 st.markdown("""
     ## DSHI Project Information ##             
-            """)
-"""In the data science course taken during the spring semester of 2025, I built prediction models
-to predict whether a Portuguese retailer's wholesale customers came from the HORECA or Retail 
-channel based on their spending on [Add specific food category - look at Jupyter notebook!!!]."""
+    In the data science course taken during the spring semester of 2025, I created a data science
+    pipeline, where I checked for missing values, renamed columns and variables. I conducted an exploratory data
+    analysis and then I built prediction models to predict whether a Portuguese retailer's wholesale
+    customers came from the HORECA or Retail channel based on annual spending on different 
+    product categories. Channel was the binary target class and the product categories Fresh, 
+    Frozen, and Delicatessen were the selected numerical features for the model. After training the models
+    with the selected features, cross validation was performed to evaluate model performance. KNN7 was found to be the best performing classifier for this domain as well as the one that 
+    performed best on this dataset. Based on the cross validation it provided the best predictions.
+                    """)
