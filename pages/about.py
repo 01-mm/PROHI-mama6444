f7ca2465a6bfcdeeaa8a307f1a61a848440b77c6
01-mm <matic.matija.mm@gmail.com>
2025-09-12 14:15:20 +0200

    Updated About.py
    Changed the name from about to About to have better formatting. Started adding some info about project from last semester. Not done yet

--- pages/about.py
+++ pages/about.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 st.title("Matija Matic")
 st.markdown("""
-    ### DSHI Project Information ###       
-            
+    ## DSHI Project Information ##      
+    In the data science course taken during the spring semester of 2025, I built prediction models to predict whether a wholesale customer was from the HORECA or Retail channel.        
             
             """)
